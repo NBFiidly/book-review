f85f02c192ce14d2bfcc7c6fc1b19e83de158d3b
nbfidly <nbfidly@gmail.com>
2025-10-26 17:37:03 +0700

    46. Displaying the Book List

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -5,30 +5,53 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 
 class Book extends Model
 {
     use HasFactory;
 
-    public function reviews()
+    public function reviews() // Untuk relasi dengan model Review
     {
         return $this->hasMany(Review::class);
     }
 
-    public function scopeTitle(Builder $query, string $title): Builder
+    public function scopeTitle(Builder $query, string $title): Builder // untuk filter berdasarkan judul buku
     {
         return $query->where('title', 'like', '%' . $title . '%');
     }
 
-    public function scopePopular(Builder $query): Builder
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder | QueryBuilder // untuk flter berdasarkan jumlah review
     {
-        return $query->withCount('reviews')
+        return $query->withCount([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ])
             ->orderBy('reviews_count', 'desc');
     }
 
-    public function scopeHighestRated(Builder $query): Builder
+    public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder | QueryBuilder // untuk filter berdasarkan rating tertinggi
     {
-        return $query->withAvg('reviews', 'rating')
+        return $query->withAvg([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ], 'rating')
             ->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    public function scopeMinReviews(Builder $query, int $minReviews): Builder | QueryBuilder // untuk filter berdasarkan jumlah minimum review
+    {
+        return $query->having('reviews_count', '>=', $minReviews);
+    }
+
+    public function dateRangeFilter(Builder $query, $from = null, $to = null): Builder | QueryBuilder // untuk filter berdasarkan rentang tanggal terbit
+    {
+        if ($from && !$to) {
+            return $query->where('created_at', '>=', $from);
+        } elseif (!$from && $to) {
+            return $query->where('created_at', '<=', $to);
+        } elseif ($from && $to) {
+            return $query->whereBetween('created_at', [$from, $to]);
+        }
+
+        return $query;
     }
 }
