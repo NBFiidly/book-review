78b7cf044ac289f55e162bb2f12788b263775964
nbfidly <nbfidly@gmail.com>
2025-10-27 01:27:16 +0700

    49. Popular or HIghest Rated - the Logic

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -11,47 +11,93 @@ class Book extends Model
 {
     use HasFactory;
 
-    public function reviews() // Untuk relasi dengan model Review
+    public function reviews()
     {
         return $this->hasMany(Review::class);
     }
 
-    public function scopeTitle(Builder $query, string $title): Builder // untuk filter berdasarkan judul buku
+    public function scopeTitle(Builder $query, string $title): Builder // untuk mencari buku berdasarkan judul
     {
-        return $query->where('title', 'like', '%' . $title . '%');
+        return $query->where('title', 'LIKE', '%' . $title . '%');
     }
 
-    public function scopePopular(Builder $query, $from = null, $to = null): Builder | QueryBuilder // untuk flter berdasarkan jumlah review
+    public function scopeWithReviewsCount(Builder $query, $from = null, $to = null): Builder|QueryBuilder // menghitung jumlah ulasan
     {
         return $query->withCount([
             'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
-        ])
-            ->orderBy('reviews_count', 'desc');
+        ]);
     }
 
-    public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder | QueryBuilder // untuk filter berdasarkan rating tertinggi
+    public function scopeWithAvgRating(Builder $query, $from = null, $to = null): Builder|QueryBuilder // menghitung rata-rata rating
     {
         return $query->withAvg([
             'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
-        ], 'rating')
+        ], 'rating');
+    }
+
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder|QueryBuilder // untuk mengurutkan berdasarkan popularitas
+    {
+        return $query->withReviewsCount()
+            ->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder|QueryBuilder // untuk mengurutkan berdasarkan rating tertinggi
+    {
+        return $query->withAvgRating()
             ->orderBy('reviews_avg_rating', 'desc');
     }
 
-    public function scopeMinReviews(Builder $query, int $minReviews): Builder | QueryBuilder // untuk filter berdasarkan jumlah minimum review
+    public function scopeMinReviews(Builder $query, int $minReviews): Builder|QueryBuilder // untuk memfilter buku dengan jumlah ulasan minimum
     {
-        return $query->having('reviews_count', '>=', $minReviews);
+        return $query->withReviewsCount()->having('reviews_count', '>=', $minReviews);
     }
 
-    public function dateRangeFilter(Builder $query, $from = null, $to = null): Builder | QueryBuilder // untuk filter berdasarkan rentang tanggal terbit
+    private function dateRangeFilter(Builder $query, $from = null, $to = null) // untuk memfilter buku berdasarkan rentang waktu
     {
         if ($from && !$to) {
-            return $query->where('created_at', '>=', $from);
+            $query->where('created_at', '>=', $from);
         } elseif (!$from && $to) {
-            return $query->where('created_at', '<=', $to);
+            $query->where('created_at', '<=', $to);
         } elseif ($from && $to) {
-            return $query->whereBetween('created_at', [$from, $to]);
+            $query->whereBetween('created_at', [$from, $to]);
         }
+    }
 
-        return $query;
+    public function scopePopularLastMonth(Builder $query): Builder|QueryBuilder // untuk mengambil buku populer bulan lalu
+    {
+        return $query->popular(now()->subMonth(), now())
+            ->highestRated(now()->subMonth(), now())
+            ->minReviews(2);
+    }
+
+    public function scopePopularLast6Months(Builder $query): Builder|QueryBuilder // untuk mengambil buku populer 6 bulan lalu
+    {
+        return $query->popular(now()->subMonths(6), now())
+            ->highestRated(now()->subMonths(6), now())
+            ->minReviews(5);
+    }
+
+    public function scopeHighestRatedLastMonth(Builder $query): Builder|QueryBuilder // untuk mengambil buku dengan rating tertinggi bulan lalu
+    {
+        return $query->highestRated(now()->subMonth(), now())
+            ->popular(now()->subMonth(), now())
+            ->minReviews(2);
+    }
+
+    public function scopeHighestRatedLast6Months(Builder $query): Builder|QueryBuilder // untuk mengambil buku dengan rating tertinggi 6 bulan lalu
+    {
+        return $query->highestRated(now()->subMonths(6), now())
+            ->popular(now()->subMonths(6), now())
+            ->minReviews(5);
+    }
+
+    protected static function booted() // untuk menghapus cache ketika buku diupdate atau dihapus
+    {
+        static::updated(
+            fn(Book $book) => cache()->forget('book:' . $book->id)
+        );
+        static::deleted(
+            fn(Book $book) => cache()->forget('book:' . $book->id)
+        );
     }
 }
